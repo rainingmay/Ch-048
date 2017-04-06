f75b5cd53ba733f1582bc26836cc966bf9253b30
gregtar <gregarty@gmail.com>
2017-04-06 18:34:29 +0300

    Add patient package

---
+++ src/main/java/pageObjects/patient/MyAppointment.java
@@ -0,0 +1,45 @@
+package pageObjects.patient;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pageObjects.allUsers.PageObject;
+/**
+ * Created by gregtar on 06.04.17.
+ */
+public class MyAppointment extends PageObject{
+    @FindBy(id = "dhx_minical_icon")
+    private WebElement calendarIcon;
+
+    @FindBy(className = "dhx_cal_today_button")
+    private WebElement todayButton;
+
+    @FindBy(className = "dhx_cal_prev_button")
+    private WebElement prevButton;
+
+    @FindBy(className = "dhx_cal_next_button")
+    private WebElement nextButton;
+
+    @FindBy(className = "back-to-top")
+    private WebElement backToTopButton;
+
+    @FindBy(xpath = "//*[@id=\"scheduler_here\"]")
+    private WebElement appointmentTable;
+
+    @FindBy(className = "dhx_cal_date")
+    private WebElement todayDateLabel;
+
+    @FindBy(name = "day_tab")
+    private WebElement dayTabButton;
+
+    @FindBy(name = "week_tab")
+    private WebElement weekTabButton;
+
+    @FindBy(name = "month_tab")
+    private WebElement monthTabButton;
+
+
+    public MyAppointment(WebDriver driver) {
+        super(driver);
+    }
+}
