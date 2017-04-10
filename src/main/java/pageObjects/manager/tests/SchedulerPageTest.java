ea79a9be06c1fdcc686d31a632060657786609a6
ytomktc <radgastt@gmail.com>
2017-04-10 16:06:42 +0300

    minor changes 0.3

--- src/main/java/pageObjects/manager/tests/SchedulerPageTest.java
+++ src/main/java/pageObjects/manager/tests/SchedulerPageTest.java
@@ -1,6 +1,7 @@
 package pageObjects.manager.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.allUsers.HospitalSeekerHomePage;
@@ -11,14 +12,17 @@ import utilities.BaseNavigation;
 public class SchedulerPageTest extends FunctionalTest{
     @Test
     public void testEventCreation() throws Exception{
+        WebDriverWait wait = new WebDriverWait(driver, 1000);
         BaseNavigation.login(driver, "manager.jh@hospitals.ua", "1111");
         HospitalsPage hospitalsPage = new HospitalsPage(driver);
         SchedulerPage schedulerPage = hospitalsPage.scheduleButtonClick(1);
         schedulerPage.setAppointment("Test",3);
+        schedulerPage.saveButtonClick();
        // BaseNavigation.logout(driver);
         Thread.sleep(6000);
-
-        driver.findElement(By.xpath("")).click();
+        System.out.println("1");
+        System.out.println(driver.findElement(By.xpath("//*[@id=\"bs-example-navbar-collapse-1\"]/ul/li[5]/a")));
+        driver.findElement(By.xpath("//*[@id=\"bs-example-navbar-collapse-1\"]/ul/li[5]/a")).click();
         driver.findElement(By.xpath("//*[@id=\"dropdawn\"]/li[2]/a/span")).click();
 
         BaseNavigation.login(driver, "manager.jh@hospitals.ua", "1111");
