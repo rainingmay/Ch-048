6ebdb9d9069a7db0d56bf6674a15370b73a6a298
Evgen Korcheviy <korcheviy.evgen21@gmail.com>
2017-04-11 17:29:28 +0300

    add test

--- src/test/pages/AdminSideTest/AdminTest.java
+++ src/test/pages/AdminSideTest/AdminTest.java
@@ -1,12 +1,16 @@
 package pages.AdminSideTest;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.admin.AllUsersPage;
 import databaseUtil.UserDAO;
 import utils.BaseNavigation;
 import utils.BaseTest;
+import utils.BrowserWrapper;
 
 
 import java.util.LinkedList;
@@ -15,10 +19,22 @@ import java.util.List;
 /**
  * Created by Evgen on 10.04.2017.
  */
-public class AdminTest extends BaseTest {
+public class AdminTest {
 
     private static final String LOGIN = "admin@hospitals.ua";
     private static final String PASSWORD = "1111";
+
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void before() {
+        driver = BrowserWrapper.browserInitialization();
+    }
+
+    @AfterTest
+    public void after() {
+        BrowserWrapper.browserClose(driver);
+    }
 
 
     @Test
@@ -47,11 +63,13 @@ public class AdminTest extends BaseTest {
             while (true) {
                 Thread.sleep(2000);
                 for (int i = 1; i < allUsersPage.gecCountOfUsersInTable(); i++) {
+                    Thread.sleep(2000);
                     List<String> actual = allUsersPage.getUserDataFromInfoWindow(i);
-                    List<String> allInfo = UserDAO.getUserFromDatabaseByEmail(actual.get(0));
+                    String email = actual.get(0);
+                    List<String> allInfo = UserDAO.getUserFromDatabaseByEmail(email);
                     List<String> expected = new LinkedList<>();
                     expected.add(allInfo.get(0));
-                    expected.add("ENABLE");
+                    expected.add("true");
                     Assert.assertEquals(actual, expected);
                 }
                 if (driver.findElements(By.cssSelector("a[aria-label='Next']")).size() == 0) break;
