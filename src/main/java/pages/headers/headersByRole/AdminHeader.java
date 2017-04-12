4f4db1f1947be845bb5d2db4fbb9873ef2d56a0a
Jeksonis <jeksonis24bryant@gmail.com>
2017-04-12 19:32:38 +0300

    fixed test class

--- src/main/java/pages/headers/headersByRole/AdminHeader.java
+++ src/main/java/pages/headers/headersByRole/AdminHeader.java
@@ -68,7 +68,6 @@ public class AdminHeader extends AuthorizedHeader {
 
     public HospitalListPage allHospitalsPage() {
         actions.click();
-        BrowserWrapper.selectDropdown(hospitalListIco, " Hospital list");
         hospitalListIco.click();
         return new HospitalListPage(driver);
     }
