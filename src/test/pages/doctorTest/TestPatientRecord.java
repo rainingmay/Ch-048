d8822d4641118297d63576c3f806df37bcf7172f
natasha <n.shtick@yandex.ua>
2017-04-18 20:47:32 +0300

    Add doctorTest

---
+++ src/test/pages/doctorTest/TestPatientRecord.java
@@ -0,0 +1,105 @@
+package pages.doctorTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.allUsers.HospitalSeekerHomePage;
+import pages.doctor.CreateNewRecordPage;
+import pages.doctor.ListPatientaPage;
+import pages.doctor.PatientsCardPage;
+import pages.headers.headersByRole.DoctorHeader;
+import pages.manager.HospitalsPage;
+import utils.BaseNavigation;
+import utils.BaseTest;
+import utils.BrowserWrapper;
+
+
+/**
+ * Created by Natasha on 18.04.2017.
+ */
+public class TestPatientRecord extends BaseTest {
+
+
+    @AfterMethod
+    public void after() {
+        try {
+            BaseNavigation.logout(this.driver);
+            BrowserWrapper.browserClose(this.driver);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            BrowserWrapper.browserClose(this.driver);
+        }
+    }
+
+
+    @Test
+
+     public void createNewRecord() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        BaseNavigation.loginAsDoctor(driver, DOCTOR_LOGIN, DOCTOR_PASSWORD);
+
+       wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/div")));
+       DoctorHeader doctorHeader = new DoctorHeader(driver);
+       doctorHeader.patientsButtonClick();
+
+        ListPatientaPage listPatientaPage = new ListPatientaPage(driver);
+        BrowserWrapper.sleep(3);
+
+        listPatientaPage.getPatientsCardClick();
+        BrowserWrapper.sleep(5);
+
+        PatientsCardPage patientsCardPage = new PatientsCardPage(driver);
+        patientsCardPage.addNewRecordButtonClick();
+        CreateNewRecordPage createNewRecord = new CreateNewRecordPage(driver);
+        createNewRecord.inputRecord("New complaint", "New result", "New prescription");
+
+        Assert.assertTrue(patientsCardPage.checkRecord());
+    }
+   /* @Test
+    public void checkPatientsSearch() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        BaseNavigation.loginAsDoctor(driver, DOCTOR_LOGIN, DOCTOR_PASSWORD);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/div")));
+        DoctorHeader doctorHeader = new DoctorHeader(driver);
+        doctorHeader.patientsButtonClick();
+        ListPatientaPage listPatientaPage = new ListPatientaPage(driver);
+        listPatientaPage.searchPatients("patient.sf@hospitals.ua");
+        BrowserWrapper.sleep(2);
+        //Assert.assertFalse(listPatientaPage.checkResultSearch());
+    }*/
+    @Test
+    public void testPatientsSortingByFirstName() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        BaseNavigation.loginAsDoctor(driver, DOCTOR_LOGIN, DOCTOR_PASSWORD);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/div")));
+        DoctorHeader doctorHeader = new DoctorHeader(driver);
+        doctorHeader.patientsButtonClick();
+        ListPatientaPage listPatientaPage = new ListPatientaPage(driver);
+        listPatientaPage.sortByFirstNameButton();
+        BrowserWrapper.sleep(5);
+
+        String firstPatientAfterSort = listPatientaPage.getDataFromTable(1, 3);
+
+        Assert.assertEquals(firstPatientAfterSort, "Charles");
+    }
+    @Test
+    public void testPatientsSortingByLastName() throws Exception {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        BaseNavigation.loginAsDoctor(driver, DOCTOR_LOGIN, DOCTOR_PASSWORD);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/div")));
+        DoctorHeader doctorHeader = new DoctorHeader(driver);
+        doctorHeader.patientsButtonClick();
+        ListPatientaPage listPatientaPage = new ListPatientaPage(driver);
+        listPatientaPage.sortByLastNameButton();
+        BrowserWrapper.sleep(5);
+
+        String firstPatientAfterSort = listPatientaPage.getDataFromTable(1, 4);
+
+        Assert.assertEquals(firstPatientAfterSort, "Auginas");
+    }
+}
