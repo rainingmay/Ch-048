d7418130f86fd9fb384bec343b8c359adb507dea
Yevhen <jeksonis24bryant@gmail.com>
2017-04-19 00:45:00 +0300

    Update AddNewHospitalPage.java

--- src/main/java/pages/admin/AddNewHospitalPage.java
+++ src/main/java/pages/admin/AddNewHospitalPage.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.support.FindBy;
 import pages.allUsers.PageObject;
 import pages.headers.headersByRole.AdminHeader;
 import utils.BrowserWrapper;
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by Jeksonis on 06.04.2017.
@@ -57,29 +60,56 @@ public class AddNewHospitalPage extends PageObject {
     @FindBy(id = "button-reset")
     private WebElement resetButton;
 
+    public void setClipboardData(String pathToPhoto) {
+        StringSelection stringSelection = new StringSelection(pathToPhoto);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+    }
+
+    public void addNewHospitalPhoto() throws AWTException {
+        setClipboardData("D:\\RhodeIslandHosp14_360_360_90.jpg");
+        Robot robot = new Robot();
+        robot.delay(1000);
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.delay(2000);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        robot.delay(1000);
+    }
+
+    public void pushAddPhotoButton() {
+        imageButton.click();
+    }
+
     public void addressData(String text) {
         addressInputField.clear();
         addressInputField.sendKeys(text);
     }
 
-    public void changeCountry(String text) {
+    public AddNewHospitalPage changeCountry(String text) {
         countryInputField.clear();
         countryInputField.sendKeys(text);
+        return new AddNewHospitalPage(driver);
     }
 
-    public void changeCity(String text) {
+    public AddNewHospitalPage changeCity(String text) {
         cityInputField.clear();
         cityInputField.sendKeys(text);
+        return new AddNewHospitalPage(driver);
     }
 
-    public void changeStreet(String text) {
+    public AddNewHospitalPage changeStreet(String text) {
         streetInputField.clear();
         streetInputField.sendKeys(text);
+        return new AddNewHospitalPage(driver);
     }
 
-    public void changeBuilding(String text) {
+    public AddNewHospitalPage changeBuilding(String text) {
         buildingInputField.clear();
         buildingInputField.sendKeys(text);
+        return new AddNewHospitalPage(driver);
     }
 
     public void addHospitalName(String text) {
@@ -121,6 +151,4 @@ public class AddNewHospitalPage extends PageObject {
        BrowserWrapper.sleep(2);
        pushSaveButton();
    }
-
-
 }
