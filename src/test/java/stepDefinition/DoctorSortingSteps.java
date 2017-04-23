e83349909f45d8dd250e87ca7dccb905c29eee24
Sanandelo <radgastt@gmail.com>
2017-04-23 12:51:27 +0300

    Doctor sort feature added

---
+++ src/test/java/stepDefinition/DoctorSortingSteps.java
@@ -0,0 +1,101 @@
+package stepDefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.*;
+
+/**
+ * Created by radgast on 23.04.17.
+ */
+public class DoctorSortingSteps {
+
+
+    @When("^Manager click on the email sorting button one time$")
+    public void manager_click_on_the_email_sorting_button_one_time() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their emails ascending$")
+    public void doctors_in_table_is_sorted_by_their_emails_ascending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their email descending$")
+    public void doctors_in_table_is_sorted_by_their_email_descending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Manager click on the first name sorting button one time$")
+    public void manager_click_on_the_first_name_sorting_button_one_time() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their first name ascending$")
+    public void doctors_in_table_is_sorted_by_their_first_name_ascending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their first name descending$")
+    public void doctors_in_table_is_sorted_by_their_first_name_descending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Manager click on the last name sorting button one time$")
+    public void manager_click_on_the_last_name_sorting_button_one_time() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their last name ascending$")
+    public void doctors_in_table_is_sorted_by_their_last_name_ascending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their last name descending$")
+    public void doctors_in_table_is_sorted_by_their_last_name_descending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Manager click on the specialization sorting button one time$")
+    public void manager_click_on_the_specialization_sorting_button_one_time() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their specialization ascending$")
+    public void doctors_in_table_is_sorted_by_their_specialization_ascending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their specialization descending$")
+    public void doctors_in_table_is_sorted_by_their_specialization_descending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Manager click on the category sorting button one time$")
+    public void manager_click_on_the_category_sorting_button_one_time() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their category ascending$")
+    public void doctors_in_table_is_sorted_by_their_category_ascending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Doctors in table is sorted by their category descending$")
+    public void doctors_in_table_is_sorted_by_their_category_descending() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
