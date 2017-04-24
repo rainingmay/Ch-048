6944fcde10064c8c8bd1149ec5ed2da37ba827d4
Sanandelo <radgastt@gmail.com>
2017-04-24 23:34:37 +0300

    xpath refactor

--- src/main/java/pages/headers/headersByRole/AdminHeader.java
+++ src/main/java/pages/headers/headersByRole/AdminHeader.java
@@ -19,42 +19,41 @@ public class AdminHeader extends AuthorizedHeader {
         super(driver);
     }
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/a")
+    @FindBy(linkText = "Actions")
     private WebElement actions;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/ul/li[2]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/admin/users?status=true\"]")
     private WebElement allUsersIco;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/ul/li[2]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/admin/newUser\"]")
     private WebElement addUserIco;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/ul/li[3]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/admin/newUser\" ]")
     private WebElement hospitalListIco;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/ul/li[4]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/admin/map/new\"]")
     private WebElement addHospitalIco;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/ul/li[5]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/admin/map/validate\"]")
     private WebElement googlePoiIco;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/ul/li[6]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/editHospitalsManagers\"]")
     private WebElement editManagers;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/ul/li[7]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/admin/configureToken\"]")
     private WebElement tokenConfig;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[4]/ul/li[8]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/admin/departmentName\"]")
     private WebElement addDepartment;
 
     @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[5]/a")
     private WebElement profile;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[5]/ul/li[1]/a")
+    @FindBy(css = "a[data-target=\"#myModal\"]")
     private WebElement myProfile;
 
-    @FindBy(xpath = "/html/body/nav/div[1]/div[2]/ul/li[5]/ul/li[2]/a")
+    @FindBy(css = "a[href=\"/HospitalSeeker/logout\"]")
     private WebElement logOut;
-
 
     public AllUsersPage allUsersPage () {
         allUsersIco.click();
