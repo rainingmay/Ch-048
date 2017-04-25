f0ff3b5f6d9b38ff48461d9bd2f23724a9df06f9
Evgen Korcheviy <korcheviy.evgen21@gmail.com>
2017-04-25 15:11:02 +0300

    dlf

---
+++ src/test/java/pages/admin/SeamTest.java
@@ -0,0 +1,14 @@
+package pages.admin;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Evgen on 25.04.2017.
+ */
+public class SeamTestExample extends SeamTest{
+
+
+    @Test
+    public void test(){}
+
+}
